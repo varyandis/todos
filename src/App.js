a092d883ad72aadbe789836e5c03f257979f3a2c
Alexandra Glen <varyandis@yandex.ru>
2025-01-27 14:15:59 +0300

    Спуск данных

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import todos from './todos';
+import TodoList from './todo-list';
 
 function App() {
+  const setDone = key => {
+    const deed = todos.find(current => current.key === key);
+    if (deed) {
+      deed.done = true
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <nav className='navbar is-light'>
+        <div className='navbar-brand'>
+          <span className='navbar-item is-uppercase'>
+            Todos
+          </span>
+        </div>
+      </nav>
+      <main className='content px-6 py-6'>
+<TodoList list={todos} setDone={setDone} />
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/todos.js
@@ -0,0 +1,23 @@
+const date1 = new Date(2024, 6, 18, 10, 24);
+const date2 = new Date(2024, 6, 19, 14, 47);
+
+const todos = [
+  {
+    title: 'Изучить React',
+    desc: 'Да поскорее!',
+    img: '',
+    done: true,
+    createdAt: date1.toLocaleDateString(),
+    key: date1.getTime(),
+  },
+  {
+    title: 'Написат ьпервое React-приложение',
+    desc: 'Список запланированных дел',
+    img: '',
+    done: false,
+    createdAt: date2.toLocaleDateString(),
+    key: date2.getTime(),
+  },
+];
+
+export default todos;
